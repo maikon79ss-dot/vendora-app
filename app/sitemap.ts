9047a2565892ac0523c9a55c8808d1b1f8495916
maikon79ss-dot <maikon79ss@gmail.com>
2026-08-27 19:35:25 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -26,6 +26,12 @@ const staticPages: MetadataRoute.Sitemap = [
     priority: 0.9,
   },
   {
+  url: `${baseUrl}/kak-da-prodavash-online`,
+  lastModified: new Date(),
+  changeFrequency: "weekly",
+  priority: 0.9,
+},
+  {
     url: `${baseUrl}/plan`,
     lastModified: new Date(),
     changeFrequency: "weekly",
